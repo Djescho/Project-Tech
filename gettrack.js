8ff48a1e98fddafee742b710ea96eb24772437a1
Djescho <djescho.mananga@hva.nl>
2021-02-07 18:02:15 +0100

    eerst upload

---
+++ gettrack.js
@@ -0,0 +1,10 @@
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    console.log(data);
+});
